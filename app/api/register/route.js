2c099cef37b577c53948c8906b5e71f57fe5917c
Adarsh Thakare <adarshythakare@gmail.com>
2025-07-13 01:36:19 +0530

    Wrapper for NextResponse and NextError

---
+++ app/api/register/route.js
@@ -0,0 +1,71 @@
+import { ZodSchema } from "zod/v3";
+import connectDB from "../../../lib/db";
+import { NextResponse } from "next/server";
+import User from "../../../models/user.model";
+import { TextEncoder } from "util";
+import { SignJWT } from "jose";
+
+export async function POST(request) {
+  try {
+    await connectDB();
+
+    // validation schema
+    const validationSchema = ZodSchema.pick({
+      name: true,
+      email: true,
+      password: true,
+    });
+
+    const payload = await request.json();
+    const validatedData = validationSchema.safeParse(payload);
+
+    if (!validatedData.success) {
+      return NextResponse.json({
+        success: false,
+        statusCode: 401,
+        message: "Invlid or Missing Input Field",
+        data: {},
+      });
+    }
+
+    const { name, email, password } = validatedData.data;
+
+    //if user already exists
+    const existingUser = await User.exists({ email });
+
+    if (existingUser) {
+      return NextResponse.json({
+        success: false,
+        statusCode: 409,
+        message: "User Already Exists!",
+        data: {},
+      });
+    }
+
+    // to register new user
+    const newUser = await User({
+      name,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    //create a token to verify the user via email
+
+    const SECRET = new TextEncoder().encode(process.env.JWT_SECRET);
+    console.log("\nJWT SECRET : ", SECRET);
+
+    const token = await new SignJWT({ userId: newUser._id })
+      // { userId: newUser._id } is the payload of the JWT — the data encoded inside the token.
+      .setIssuedAt()
+      //Sets the "iat" (Issued At) claim to the current timestamp. This helps track when the token was generated.
+      .setExpirationTime("1hr")
+      //Sets the "exp" (Expiration Time) claim
+      .setProtectedHeader({ alg: "HS256" })
+      //🔹 Sets the JWT header, which includes metadata about how the token is signed.
+      //🔹 { alg: "HS256" } means the token is signed using HMAC SHA-256 (symmetric encryption). It needs a secret key to both sign and verify.
+      .sign(SECRET);
+    //This signs the token using the given secret and returns the final JWT string (which you’ll usually send to the frontend or store in a cookie).
+  } catch (err) {}
+}
---
+++ lib/requestHelper.ts
@@ -0,0 +1,56 @@
+import { NextResponse } from "next/server";
+
+type ResponseProp = {
+  success: boolean;
+  statusCode: number;
+  message: string;
+  data: object;
+};
+
+type ErrorProp = {
+  customMessage: string;
+  error: any;
+};
+
+export const response = ({
+  success,
+  statusCode,
+  message,
+  data = {},
+}: ResponseProp) => {
+  return NextResponse.json({
+    success,
+    statusCode,
+    message,
+    data,
+  });
+};
+
+export const catchError = ({ error, customMessage }: ErrorProp) => {
+  //handling duplocate key error
+
+  if (error.code === 110000) {
+    const keys = Object.keys(error.keyPattern).join(",");
+    error.message = `Duplicate Fields : ${keys} . \n These Fields value must be unique`;
+  }
+
+  let errorObj = {};
+
+  if (process.env.NODE_ENV === "development") {
+    errorObj = {
+      message: error.message,
+      error,
+    };
+  } else {
+    errorObj = {
+      message: customMessage || "Internal Server Error",
+    };
+  }
+
+  return response({
+    success: false,
+    statusCode: 500,
+    message: error.code,
+    data: errorObj,
+  });
+};
